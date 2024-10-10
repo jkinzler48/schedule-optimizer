82d41825ece3c9ba328c84a000984751a1a6abcd
Jake Kinzler <jakekinzler@48s-MacBook-Pro.local>
2024-10-10 19:42:16 -0400

    updated App.js according to kickoff solution"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@
 import React from 'react';
 import './App.css';
 import Main from './Components/Main.js'; // Import the Main component
+import * as Env from "./environments";
+import Parse from "parse";
+
+// Initialize Parse
+Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
+Parse.serverURL = Env.SERVER_URL;
 
 function App() {
   return (
